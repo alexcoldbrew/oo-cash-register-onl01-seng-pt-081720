5c2e3c8ecfbe8e14a8639a5820dbf5fc192802ad
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-14 02:25:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,4 +4,24 @@ class CashRegister
     @total = 0
   end
   
+  def total
+  @total
+  end
+  
+  def add_item
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
 end
