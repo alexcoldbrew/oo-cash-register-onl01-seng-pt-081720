3b5d141ccf3976158c5f17648ddbfba76067af01
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-14 03:12:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,8 @@ class CashRegister
     if quantity > 1
       i = 0
       while i < quantity
-      
+        @items << title
+        i += 1
     @total += (price * quantity)
     @items << title
   end
