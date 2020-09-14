8ef5d87516cae4014b02134f8fed77040f0badd5
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-14 03:03:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,9 +16,13 @@ class CashRegister
   end
   
   def apply_discount
-    @discount = @discount/100.to_f
-    @total = @total - (@total * @discount)
-    "After the discount, the total comes to $#{@total.to_i}."
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * @discount)
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    
   end
   
   def items
